c7aeb200551bd5dfdbf0469b48d9c77cd3d5eb11
Lucas Picazio <l.araujopicazio@gmail.com>
2020-07-02 02:32:45 -0300

    finalizando compilador

--- src/Main/Compiler.cs
+++ src/Main/Compiler.cs
@@ -51,7 +51,7 @@ namespace Main
                         case OperationEnum.ORI:
                         case OperationEnum.LW:
                         case OperationEnum.LUI:
-                        case OperationEnum.ADDI://Perguntar: No ep disse que era para usar ADD com constante mas não tem como
+                        case OperationEnum.ADDI:
                             result.Add(ImediateFormat(operation, parts));
                             break;
                         case OperationEnum.MOVE:
@@ -71,8 +71,8 @@ namespace Main
         private BitArray PseudoFormat(OperationEnum operation, List<string> parts)
         {
             var result = new BitArray(32);
-            if (operation == OperationEnum.LUI)
-                result = LuiOperation(parts);
+            if (operation == OperationEnum.LI)
+                result = LiOperation(parts);
             else
                 result = MoveOperation(parts);
 
@@ -85,9 +85,30 @@ namespace Main
             return RegisterFormat(OperationEnum.ADD, new List<string> { "ADD", parts[1], "$zero", parts[2] });
         }
 
-        private BitArray LuiOperation(List<string> parts)
+        private BitArray LiOperation(List<string> parts)
         {
-            throw new NotImplementedException();
+            var result = new BitArray(32);
+            if(Int16.TryParse(parts[2], out Int16 resultParse))// Menor que 16 bits
+            {
+                return RegisterFormat(OperationEnum.ADDI, new List<string> { "ADDI", parts[1], "$zero", resultParse.ToString() });
+            }
+            else if(Int32.TryParse(parts[2], out Int32 resultParse32))
+            {
+                var hexValue = resultParse32.ToString("X");
+      
+                if (hexValue.Count() % 2 != 0)// se for impar adiciona um 0
+                    hexValue = "0" + hexValue;
+
+                string part1 = hexValue.Substring(0, hexValue.Length / 2);
+                string part2 = hexValue.Substring(hexValue.Length / 2, hexValue.Length / 2);
+
+                result.Append(ImediateFormat(OperationEnum.LUI, new List<string> { "LUI", parts[1], part1 }));
+                result.Append(ImediateFormat(OperationEnum.ORI, new List<string> { "ORI", parts[1], part2 }));
+
+            }
+
+            return result;
+
         }
 
         private BitArray RegisterFormat(OperationEnum operation, List<string> parts)
@@ -106,6 +127,9 @@ namespace Main
             var register3String = parts[2].Trim(new char[] { '$', ',' });
             if (Enum.TryParse(register3String, true, out RegisterEnum register3))
                 result.Append(new BitArray((int)register3));
+
+            else if (Int16.TryParse(parts[2], out short resultParse)) // valor constante
+                result.Append(new BitArray(resultParse));
 
             result = result.Append(new BitArray(5, false));
 
@@ -141,7 +165,6 @@ namespace Main
 
             if (Int32.TryParse(parts[3], out int refe))
                 result = result.Append(new BitArray(refe));
-
 
             return result;
         }
